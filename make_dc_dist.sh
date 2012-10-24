9ae38df7a496c01c03f4c0de15b0277d26249dd6
Michael Galloy <mgalloy@idldev.com>
2012-10-24 16:44:46 -0600

    Adding a script to create Doc Center output and package it to give to Exelis VIS.

---
+++ make_dc_dist.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# make the Doc Center docs
+make dcdoc
+
+# remove ls.pro and reference to it
+rm -f api-dcdocs/ls.html
+sed '/ls.html/d' api-dcdocs/idldoc-index.csv > api-dcdocs/mglib-index.csv
+rm -f api-dcdocs/idldoc-index.csv
+
+# delete old zip file
+rm -f mglib-doccenter.tar.gz
+rm -rf mglib-doccenter
+
+# create the zip file
+cp -r api-dcdocs mglib-doccenter
+tar cfzv mglib-doccenter.tar.gz mglib-doccenter
+rm -rf mglib-doccenter
