edd87746e2717029df4d43d6858899b9562ca297
Michael Galloy <mgalloy@txcorp.com>
2013-03-12 12:04:40 -0600

    Can't seem to build docs with out of source build, so changing to an in-source build.

--- example_configure.sh
+++ example_configure.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# using static libtre.a requires adding an --enable-static flag to the tre
+# formula in /usr/local/Library/Formula/tre.rb
+
 cmake \
   -DCMAKE_INSTALL_PREFIX=~/projects/mglib-install \
   -DMARKDOWN_INCLUDE_DIR=/usr/local/include \
   -DMARKDOWN_LIBRARY=/usr/local/lib/libmarkdown.a \
   -DTRE_INCLUDE_DIR=/usr/local/include \
-  -DTRE_LIBRARY=/usr/local/lib/libtre.dylib \
+  -DTRE_LIBRARY=/usr/local/lib/libtre.a \
   -DGSL_INCLUDE_DIR=/usr/local/include/gsl \
   -DGSL_LIBRARY_DIR=/usr/local/lib \
-  ../mglib
+  .
 
