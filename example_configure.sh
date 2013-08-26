470b38826a776a3c0b5f641774f11a07cf1f9341
Michael Galloy <mgalloy@gmail.com>
2013-08-26 10:27:07 -0600

    Installing in ~/software and building in build/.

--- example_configure.sh
+++ example_configure.sh
@@ -3,13 +3,17 @@
 # using static libtre.a requires adding an --enable-static flag to the tre
 # formula in /usr/local/Library/Formula/tre.rb
 
+rm -rf build
+mkdir build
+cd build
+
 cmake \
-  -DCMAKE_INSTALL_PREFIX=~/projects/mglib-install \
+  -DCMAKE_INSTALL_PREFIX=~/software/mglib \
   -DMARKDOWN_INCLUDE_DIR=/usr/local/include \
   -DMARKDOWN_LIBRARY=/usr/local/lib/libmarkdown.a \
   -DTRE_INCLUDE_DIR=/usr/local/include \
   -DTRE_LIBRARY=/usr/local/lib/libtre.a \
   -DGSL_INCLUDE_DIR=/usr/local/include \
   -DGSL_LIBRARY_DIR=/usr/local/lib \
-  .
+  ..
 
