54e877368c1156ad181876fdcf6f6ec128076a92
Michael Galloy <mgalloy@gmail.com>
2014-06-25 14:30:28 -0600

    Actually scp'ing userdocs to docs.idldev.com.

--- upload_userdoc.sh
+++ upload_userdoc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ -d $1/api-userdocs ]; then
-  echo "scp -r $1/api-userdocs docs.idldev.com/mglib"
+  cmd="scp -r $1/api-userdocs idldev.com:~/docs.idldev.com/mglib"
+  $cmd
 else
   echo "run 'make userdoc' before uploading"
 fi
