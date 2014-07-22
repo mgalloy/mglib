194d5b3d726469a4ab12df04ad4971bbcebf48e8
Michael Galloy <mgalloy@gmail.com>
2014-07-22 14:55:44 -0600

    Using IDL_PTRINT instead of unsigned long in GSL binding header files.

--- src/gsl/mg_gsl_rng_bindings.h
+++ src/gsl/mg_gsl_rng_bindings.h
@@ -1,10 +1,10 @@
 void gsl_rng_env_setup();
-unsigned long gsl_rng_alloc(unsigned long T);
-void gsl_rng_free (unsigned long r);
+IDL_PTRINT gsl_rng_alloc(IDL_PTRINT t);
+void gsl_rng_free(IDL_PTRINT r);
 
-void gsl_rng_set(unsigned long r, unsigned long seed);
+void gsl_rng_set(IDL_PTRINT r, unsigned long seed);
 
-unsigned long gsl_rng_get(unsigned long r);
-double gsl_rng_uniform(unsigned long r);
-double gsl_rng_uniform_pos(unsigned long r);
-unsigned long gsl_rng_uniform_int(unsigned long r, unsigned long n);
+unsigned long gsl_rng_get(IDL_PTRINT r);
+double gsl_rng_uniform(IDL_PTRINT r);
+double gsl_rng_uniform_pos(IDL_PTRINT r);
+unsigned long gsl_rng_uniform_int(IDL_PTRINT r, unsigned long n);
