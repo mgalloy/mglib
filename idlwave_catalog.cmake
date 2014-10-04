0117e9cf062bb28be9f5b611692fac5a34608951
Michael Galloy <mgalloy@gmail.com>
2014-10-04 09:46:53 -0600

    Automatically run idlwave_catalog on installed (including packaged) lib/ directory.

--- idlwave_catalog.cmake
+++
@@ -1,7 +0,0 @@
-#execute_process(COMMAND ${idlwave_DIR}/idlwave_catalog -v mglib
-#                WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib)
-
-message(STATUS ${idlwave_DIR}/idlwave_catalog -v mglib)
-message(STATUS ${CMAKE_INSTALL_PREFIX}/lib)
-
-message(STATUS "CPACK_PACKAGE_DIRECTORY = ${CPACK_PACKAGE_DIRECTORY}")
