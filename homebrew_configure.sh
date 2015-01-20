d8dedece4f59ab7d503a8e86cfda34a88522b391
Michael Galloy <mgalloy@gmail.com>
2015-01-20 13:07:54 -0700

    Specifying type of CMake options.

--- homebrew_configure.sh
+++ homebrew_configure.sh
@@ -8,17 +8,17 @@ mkdir build
 cd build
 
 cmake \
-  -DCMAKE_INSTALL_PREFIX=~/software/mglib \
-  -DMARKDOWN_INCLUDE_DIR=/usr/local/include \
-  -DMARKDOWN_LIBRARY=/usr/local/lib/libmarkdown.a \
-  -DTRE_INCLUDE_DIR=/usr/local/include \
-  -DTRE_LIBRARY=/usr/local/lib/libtre.a \
-  -DGSL_INCLUDE_DIR=/usr/local/include \
-  -DGSL_LIBRARY_DIR=/usr/local/lib \
-  -DZEROMQ_INCLUDE_DIR=/usr/local/include \
-  -DZEROMQ_LIBRARY_DIR=/usr/local/lib \
-  -DIDLdoc_DIR=~/projects/idldoc/src \
-  -Dmgunit_DIR=~/projects/mgunit/src \
-  -Didlwave_DIR=~/software/idlwave \
+  -DCMAKE_INSTALL_PREFIX:FILEPATH=~/software/mglib \
+  -DMARKDOWN_INCLUDE_DIR:FILEPATH=/usr/local/include \
+  -DMARKDOWN_LIBRARY:FILEPATH=/usr/local/lib/libmarkdown.a \
+  -DTRE_INCLUDE_DIR:FILEPATH=/usr/local/include \
+  -DTRE_LIBRARY:FILEPATH=/usr/local/lib/libtre.a \
+  -DGSL_INCLUDE_DIR:FILEPATH=/usr/local/include \
+  -DGSL_LIBRARY_DIR:FILEPATH=/usr/local/lib \
+  -DZEROMQ_INCLUDE_DIR:FILEPATH=/usr/local/include \
+  -DZEROMQ_LIBRARY_DIR:FILEPATH=/usr/local/lib \
+  -DIDLdoc_DIR:FILEPATH=~/projects/idldoc/src \
+  -Dmgunit_DIR:FILEPATH=~/projects/mgunit/src \
+  -Didlwave_DIR:FILEPATH=~/software/idlwave \
   ..
 
